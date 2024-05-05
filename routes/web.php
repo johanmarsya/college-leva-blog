ae077217d536ee085ada1b6c7500792077ba6fb0
johanmarsya <johanmarsya1306@gmail.com>
2024-05-06 02:28:26 +0900

    8-4 is completed!

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use App\Http\Controllers\PostController; //外部にあるPostControllerクラ�
 */
 
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
